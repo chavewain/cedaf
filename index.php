21ff116c939e1d3fcaad0b7dffe4bf1091b03330
chavewain <dionisiochavez@gmail.com>
2021-10-22 12:19:40 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,284 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Agency - One Page Bootstrap Template</title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/css/style.css">
+
+
+</head>
+
+<body>
+
+    <!--====== HEADER ONE PART START ======-->
+
+    <!-- our markup -->
+    <section id="inicio">
+        <div class="header-content-area">
+            <header>
+                <div class="navbar-area">
+                    <div class="container">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-transparent p-0">
+
+
+                            <a href="#" class="navbar-brand" style="display:contents!important">
+                                <img style="max-width: 140px;" src="<?= get_template_directory_uri(); ?>/assets/images/logo.svg" alt="Logo">
+                            </a>
+                            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarCollapse">
+                                <div class="navbar-nav ms-auto">
+                                    <a href="#inicio" class="nav-item nav-link active">Inicio</a>
+                                    <a href="#proyecto" class="nav-item nav-link">Proyecto</a>
+                                    <a href="#donante" class="nav-item nav-link">Donante</a>
+                                    <a href="#empacadoras" class="nav-item nav-link">Empacadoras</a>
+                                    <a href="#" class="nav-item nav-link disabled" tabindex="-1">Acceso</a>
+                                </div>
+                                <!-- <div class="navbar-nav ms-auto">
+                                <a href="#" class="nav-item nav-link">Login</a>
+                            </div> -->
+                            </div>
+
+                        </nav>
+                    </div>
+                </div>
+
+
+            </header>
+            <div class="header-shape">
+                <img src="<?= get_template_directory_uri(); ?>/assets/images/header-shape.svg" alt="shape">
+            </div>
+
+            <div class="container">
+                <div class="header-wrapper">
+                    <div class="header-content">
+                        <h3 class="header-title">Proyecto de Fortalecimiento
+                            de la Calidad para el Desarrollo
+                            de las Mipimes de la Cadena
+                            de Valor de Frutas Procesadas</h3>
+                        <p class="text">Proyecto Financiado con FOndos de la Union Europea
+                            No. CRIS FED / 2019 / 409-015</p>
+                        <!-- <div class="header-btn rounded-buttons">
+                        <a class="main-btn rounded-one" href="https://google.com">Purchase Now</a>
+                    </div> -->
+
+                    </div> <!-- header content -->
+
+                    <div class="header-image d-none d-lg-block">
+                        <div class="image">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/pina.fw.png" alt="Header">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="proyecto">
+        <div class="container mt-4 bg-gray">
+            <div class="row">
+                <div class="col-sm-12 p-4">
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/board.png" alt="User">
+                        </div>
+                        <div class="col-md-9">
+
+                            <div class="col-12 pb-3">
+                                <h3 class="mb-0">Sobre el Proyecto</h3>
+                            </div>
+
+
+                            <p>
+                                El Proyecto “Fortalecimiento de la Calidad para el Desarrollo de las Mipymes de la Cadena de Valor de Frutas Procesadas” busco incrementar la competitividad de la cadena de valor de frutas procesadas, de los rubros de mango, piña y aguacate, por medio del fortalecimiento de la Calidad y la Inocuidad Alimentaria fruto de la implementación Buenas Prácticas Agrícolas (BPA) y de Manufactura (BPM), así como también las que promueven la conservación y protección del Medio Ambiente.
+                            </p>
+                            <p>
+                                Este proyecto fue implementado por el Ministerio de Industria, Comercio y Mipymes (MICM) mediante financiamiento por la Unión Europea (UE) y ejecutado por un consorcio de instituciones, conformado por la Universidad ISA (Coordinador principal, la Fundación CODESPA, el Centro para el Desarrollo Agropecuario y Forestal (CEDAF) y la Junta Agroempresarial Dominicana (JAD).
+                            </p>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <section id="donante">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-sm-12 p-4">
+
+                    <div class="row">
+
+                        <div class="col-md-12">
+
+                            <div class="col-12 pb-3">
+                                <h3 class="mb-0">Donante e implementador</h3>
+                            </div>
+
+
+                            <p>
+                                La Unión Europea (UE) es una asociación económica y política única en el mundo, formada por 28 países. La cual cuenta con una moneda única, una bandera, un himno y el día de Europa, que se celebra cada 9 de mayo.
+                            </p>
+                            <p>
+                                La UE es el mayor donante de ayuda internacional del mundo: aporta más de 50.000 millones de euros al año para acabar con la pobreza y promover el desarrollo global. Se ha comprometido a aplicar los convenios internacionales sobre la eficacia de la ayuda y a rendir cuentas ante los ciudadanos de la UE, que hacen posibles las iniciativas solidarias. https://europa.eu/
+                            </p>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <section id="patrocinadores">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-sm-12 p-4">
+
+                    <div class="row">
+
+                        <div class="col-md-12">
+
+                            <div class="col-12 pb-3">
+                                <h3 class="mb-0">Patrocinadores</h3>
+                            </div>
+
+
+                            <div class="row mx-auto my-auto justify-content-center">
+                                <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+                                    <div class="carousel-inner" role="listbox">
+                                        <div class="carousel-item active">
+                                            <div class="col-md-3 d-flex align-items-center">
+                                                <div class="card border-0 ">
+                                                    <div class=" card-img ">
+                                                        <img src="<?= get_template_directory_uri(); ?>/assets/images/micm.png" alt="Logo" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="col-md-3 d-flex align-items-center">
+                                                <div class="card border-0 ">
+                                                    <div class=" card-img ">
+                                                        <img src="<?= get_template_directory_uri(); ?>/assets/images/isa.png" alt="Logo" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="col-md-3 d-flex align-items-center">
+                                                <div class="card border-0 ">
+                                                    <div class=" card-img ">
+                                                        <img src="<?= get_template_directory_uri(); ?>/assets/images/codespa.png" alt="Logo" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="col-md-3 d-flex align-items-center">
+                                                <div class="card border-0 ">
+                                                    <div class=" card-img ">
+                                                        <img src="<?= get_template_directory_uri(); ?>/assets/images/cedaf.png" alt="Logo" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="col-md-3 d-flex align-items-center">
+                                                <div class="card border-0 ">
+                                                    <div class=" card-img ">
+                                                        <img src="<?= get_template_directory_uri(); ?>/assets/images/jad.png" alt="Logo" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="col-md-3 d-flex align-items-center">
+                                                <div class="card border-0 ">
+                                                    <div class=" card-img ">
+                                                        <img src="<?= get_template_directory_uri(); ?>/assets/images/ue.png" alt="Logo" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    </a>
+                                    <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    </a>
+                                </div>
+                            </div>
+                           
+
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+    </section>
+    <!-- import jQuery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- write script to toggle class on scroll -->
+    <script>
+        let items = document.querySelectorAll('.carousel .carousel-item')
+
+        items.forEach((el) => {
+            const minPerSlide = 4
+            let next = el.nextElementSibling
+            for (var i = 1; i < minPerSlide; i++) {
+                if (!next) {
+                    // wrap carousel by using first child
+                    next = items[0]
+                }
+                let cloneChild = next.cloneNode(true)
+                el.appendChild(cloneChild.children[0])
+                next = next.nextElementSibling
+            }
+        })
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 1) {
+                $('header').addClass("sticky");
+            } else {
+                $('header').removeClass("sticky");
+            }
+        });
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
